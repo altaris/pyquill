a40718bd64d640f5396c23d0f2e303a9c6a0bdc7
Cédric HT <altaris@users.noreply.github.com>
2024-07-26 13:07:02 +0900

    Despaghettification

--- pyquill/render.py
+++ pyquill/render.py
@@ -10,6 +10,32 @@ from qiskit.circuit import Qubit
 from qiskit.dagcircuit import DAGOpNode
 
 
+def _min_qarg(
+    node: DAGOpNode, qubits_abs_idx: dict[Qubit, int], offset: int = 0
+) -> tuple[Qubit, int]:
+    """
+    Return the input qubit with the minimum absolute index, and said index. If
+    `offset` is specified, only consider qargs starting from that index.
+    """
+    min_q, min_ai = node.qargs[offset], qubits_abs_idx[node.qargs[offset]]
+    for q in node.qargs[offset:]:
+        ai = qubits_abs_idx[q]
+        if ai < min_ai:
+            min_q, min_ai = q, ai
+    return min_q, min_ai
+
+
+def _n_wires(
+    node: DAGOpNode, qubits_abs_idx: dict[Qubit, int], offset: int = 0
+) -> int:
+    """
+    Return the width of the gate, i.e. the number of wires it spans over. If
+    `offset` is specified, only consider qargs starting from that index.
+    """
+    iai = [qubits_abs_idx[q] for q in node.qargs[offset:]]
+    return max(iai) - min(iai) + 1
+
+
 def as_fraction_of_pi(theta: float) -> str:
     """
     Represents a float as an integer fraction of pi, as a typst math string.
@@ -87,7 +113,7 @@ def easy_op_to_typst(op_name: str, parameters: list) -> str:
 
 
 def render_opnode(
-    node: DAGOpNode, indices: dict[Qubit, int]
+    node: DAGOpNode, qubits_abs_idx: dict[Qubit, int]
 ) -> dict[Qubit, str]:
     """
     Given an opnode and a mapping of qubits to their absolute indices, returns
@@ -96,68 +122,41 @@ def render_opnode(
 
     Args:
         node (DAGOpNode):
-        indices (dict[Qubit, int]): Mapping of qubits to their
+        qubits_abs_idx (dict[Qubit, int]): Mapping of qubits to their
             absolute indices.
 
     Returns:
         dict[Qubit, str]:
     """
-
-    def _min_iq() -> Qubit:
-        """
-        Input qubit whose absolude index is minimal among the absolute index of
-        all input qubits.
-        """
-        return {v: k for k, v in indices.items()}[_min_iqai()]
-
-    def _min_iqai() -> int:
-        """Minimum absolute index of input qubits"""
-        return min(indices[q] for q in node.qargs)
-
-    def _q(qarg_idx: int) -> Qubit:
-        """Node's input qubit at the given index"""
-        return node.qargs[qarg_idx]
-
-    def _qai(qarg_idx: int) -> int:
-        """Converts a node's qarg index to an absolute index"""
-        return indices[node.qargs[qarg_idx]]
-
     result: dict[Qubit, str] = {}
 
     if node.name == "cz":
-        tgt = _qai(1) - _qai(0)
-        result[_q(0)], result[_q(1)] = f"ctrl({tgt})", "ctrl(0)"
+        q0, q1 = node.qargs[:2]
+        ri = qubits_abs_idx[q1] - qubits_abs_idx[q0]
+        result[q0], result[q1] = f"ctrl({ri})", "ctrl(0)"
 
     elif node.name == "cp":
-        tgt, theta = _qai(1) - _qai(0), as_fraction_of_pi(node.op.params[0])
-        result[_q(0)] = f"ctrl({tgt}, wire-label: ${theta}$)"
-        result[_q(1)] = "ctrl(0)"
+        q0, q1 = node.qargs[:2]
+        ri = qubits_abs_idx[q1] - qubits_abs_idx[q0]
+        theta = as_fraction_of_pi(node.op.params[0])
+        result[q0] = f"ctrl({ri}, wire-label: ${theta}$)"
+        result[q1] = "ctrl(0)"
 
     elif node.name == "p":
         theta = as_fraction_of_pi(node.op.params[0])
-        result[_q(0)] = f"phase(${theta}$)"
+        result[node.qargs[0]] = f"phase(${theta}$)"
 
     elif node.name == "rzz":
-        tgt, theta = _qai(1) - _qai(0), as_fraction_of_pi(node.op.params[0])
-        result[_q(0)] = f"ctrl({tgt}, wire-label: $Z Z ({theta})$)"
-        result[_q(1)] = "ctrl(0)"
+        q0, q1 = node.qargs[:2]
+        ri = qubits_abs_idx[q1] - qubits_abs_idx[q0]
+        theta = as_fraction_of_pi(node.op.params[0])
+        result[q0] = f"ctrl({ri}, wire-label: $Z Z ({theta})$)"
+        result[q1] = "ctrl(0)"
 
     elif node.name == "swap":
-        tgt = _qai(1) - _qai(0)
-        result[_q(0)], result[_q(1)] = f"swap({tgt})", "targX()"
-
-    elif node.name.startswith("cc"):  # two qubits controlled gate
-        iai = [indices[q] for q in node.qargs[2:]]
-        width = max(iai) - min(iai) + 1
-        tgt_0, tgt_1 = _qai(2) - _qai(0), _qai(2) - _qai(1)
-        name = node.name[2:].upper()
-        result[_q(0)], result[_q(1)] = f"ctrl({tgt_0})", f"ctrl({tgt_1})"
-        if name == "X":
-            result[_q(2)] = "targ()"
-        elif name == "Z":
-            result[_q(2)] = "ctrl(0)"
-        else:
-            result[_q(2)] = f"mqgate(${name}$, n: {width}, width: 5em)"
+        q0, q1 = node.qargs[:2]
+        ri = qubits_abs_idx[q1] - qubits_abs_idx[q0]
+        result[q0], result[q1] = f"swap({ri})", "targX()"
 
     elif node.name.startswith("c"):  # controlled gate
         if node.name.startswith("cc"):
@@ -166,35 +165,35 @@ def render_opnode(
             n_contols, gate = int(match.group(1)), match.group(2)
         else:
             n_contols, gate = 1, node.name[1:]
-        # input qubit absolute indices
-        iai = [indices[q] for q in node.qargs[n_contols:]]
-        width = max(iai) - min(iai) + 1
-        for i in range(n_contols):
-            tgt = min(iai) - _qai(i)
-            result[_q(i)] = f"ctrl({tgt})"
+        min_in_q, min_in_q_ai = _min_qarg(node, qubits_abs_idx, n_contols)
+        n_wires = _n_wires(node, qubits_abs_idx, n_contols)
+        for q_ctrl in node.qargs[:n_contols]:
+            tgt = min_in_q_ai - qubits_abs_idx[q_ctrl]
+            result[q_ctrl] = f"ctrl({tgt})"
         if gate == "swap":
-            swp = _qai(2) - _qai(1)
-            result[_q(1)], result[_q(2)] = f"swap({swp})", "targX()"
+            q1, q2 = node.qargs[n_contols : n_contols + 2]
+            swp = qubits_abs_idx[q2] - qubits_abs_idx[q1]
+            result[q1], result[q2] = f"swap({swp})", "targX()"
         elif gate == "x":
-            result[_q(-1)] = "targ()"
+            result[node.qargs[-1]] = "targ()"
         else:
-            t = easy_op_to_typst(gate, node.op.params)
-            result[_q(n_contols)] = f"mqgate({t}, n: {width}, width: 5em)"
+            tpst = easy_op_to_typst(gate, node.op.params)
+            result[min_in_q] = f"mqgate({tpst}, n: {n_wires}, width: 5em)"
 
     else:  # Generic gate
-        iai = [indices[q] for q in node.qargs]
-        width = max(iai) - min(iai) + 1
-        name = easy_op_to_typst(node.name, node.op.params)
-        if width == 1:
-            result[_min_iq()] = f"mqgate({name}, n: {width}, width: 5em)"
+        n_wires = _n_wires(node, qubits_abs_idx)
+        tpst = easy_op_to_typst(node.name, node.op.params)
+        min_qarg, min_qarg_ai = _min_qarg(node, qubits_abs_idx)
+        if n_wires == 1:
+            result[min_qarg] = tpst
         else:
             inputs = []
             for i, q in enumerate(node.qargs):
-                j = indices[q] - _min_iqai()
+                j = qubits_abs_idx[q] - min_qarg_ai
                 inputs.append(f'(qubit: {j}, label: "{i}")')
-            a = ", ".join(inputs)
-            result[_min_iq()] = (
-                f"mqgate({name}, n: {width}, inputs: ({a}), width: 5em)"
+            clause = ", ".join(inputs)
+            result[min_qarg] = (
+                f"mqgate({tpst}, n: {n_wires}, inputs: ({clause}), width: 5em)"
             )
 
     return result
